50e45b2ecdcce8e75bd0332952fa684ead8d4ad7
mathuria <mathuria@adobe.com>
2024-07-30 17:39:41 +0530

    Prevent default to avoid jump and url change

--- unitylibs/core/steps/app-connector.js
+++ unitylibs/core/steps/app-connector.js
@@ -31,7 +31,7 @@ async function continueInApp(cfg, appName, btnConfig) {
   const continuebtn = unityWidget.querySelector(`continue-in-${appName}`);
   if (continuebtn) return continuebtn;
   const btn = await createActionBtn(btnConfig, `continue-in-app continue-in-${appName}`, true, true);
-  btn.addEventListener('click', async () => {
+  btn.addEventListener('click', async (evt) => {
     evt.preventDefault();
     const data = getPreludeData(cfg);
     const connectorOptions = {
