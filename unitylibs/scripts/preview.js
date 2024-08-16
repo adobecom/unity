b54ea1f436d57b11ee793e187ae42a09ed9ead4a
mathuria <mathuria@adobe.com>
2024-08-16 11:30:02 +0530

    preview.html

--- unitylibs/scripts/preview.js
+++ unitylibs/scripts/preview.js
@@ -142,6 +142,7 @@ const miloLibs = setLibs(LIBS);
 (async function listenAndReload() {
   window.addEventListener("message", async (event) => {
     const { unity } = JSON.parse(event.data);
+    console.log(unity);
     const u = document.querySelector('.unity');
     const parser = new DOMParser();
     const ih = parser.parseFromString(unity, "text/html");
