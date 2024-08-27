82b0a62cc36936e4907ae8b981cf8624b601ad2c
mathuria <mathuria@adobe.com>
2024-08-27 19:37:59 +0530

    Initial Refactor changes

--- unitylibs/blocks/unity/unity.js
+++ unitylibs/blocks/unity/unity.js
@@ -1,15 +1,19 @@
 import { loadStyle } from '../../scripts/utils.js';
+
 function getUnityLibs(prodLibs, project = 'unity') {
+  let libs = '';
   const { hostname, origin } = window.location;
-  if (project === 'unity') { return `${origin}/unitylibs`; }
+  if (project === 'unity') { libs = `${origin}/unitylibs`; return libs; }
   if (!hostname.includes('hlx.page')
     && !hostname.includes('hlx.live')
     && !hostname.includes('localhost')) {
-    return prodLibs;
+    libs = prodLibs;
+    return libs;
   }
   const branch = new URLSearchParams(window.location.search).get('unitylibs') || 'main';
-  if (branch.indexOf('--') > -1) return `https://${branch}.hlx.live/unitylibs`;
-  return `https://${branch}--unity--adobecom.hlx.live/unitylibs`;
+  if (branch.indexOf('--') > -1) { libs = `https://${branch}.hlx.live/unitylibs`; return libs; }
+  libs = `https://${branch}--unity--adobecom.hlx.live/unitylibs`;
+  return libs;
 }
 
 export default async function init(el) {
