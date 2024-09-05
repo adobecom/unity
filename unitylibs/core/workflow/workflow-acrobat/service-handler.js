b32a287a270a0a40f34a0eedbb209fa4a97fb5ea
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-09-04 17:59:21 -0700

    remove test code

--- unitylibs/core/workflow/workflow-acrobat/service-handler.js
+++ unitylibs/core/workflow/workflow-acrobat/service-handler.js
@@ -4,7 +4,7 @@
 /* eslint-disable no-restricted-syntax */
 
 import {
-  getGuestAccessToken, showErrorToast,
+  getGuestAccessToken,
 } from '../../../scripts/utils.js';
 
 export default class ServiceHandler {
