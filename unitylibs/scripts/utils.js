a80ab1175b78a05dc0257c3f2be2ee1b26921f00
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-10-10 13:33:17 -0700

    Update utils.js

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -143,7 +143,7 @@ export async function priorityLoad(parr) {
         console.error(`Fetch failed for ${p}:`, err);
         throw err;
       });
-      promiseArr.push(fetchPromise);
+      promiseArr.push([fetchPromise]);
     }
   });  
   try {
