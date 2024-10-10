c1f491f87a2f77daec75238111b14a874d702f8e
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-10-10 13:16:58 -0700

    Update utils.js

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -148,7 +148,7 @@ export async function priorityLoad(parr) {
   });  
   try {
     console.log('Promises:', promiseArr);
-    await Promise.all([...promiseArr, timeoutPromise(10000)]);
+    await Promise.all(promiseArr);
   } catch (e) {
     console.error('Error in Promise.all:', e);
     throw e;
