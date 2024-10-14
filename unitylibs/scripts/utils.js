0b241b70dc7a9e6cde740c6dfd546c770f153c91
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-10-14 09:07:12 -0700

    Update utils.js

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -124,14 +124,54 @@ export async function createActionBtn(btnCfg, btnClass, iconAsImg = false, swapO
   return actionBtn;
 }
 
+export function loadLinks(href, { as, callback, crossorigin, rel, fetchpriority } = {}) {
+  return new Promise((resolve, reject) => {
+    let element;
+
+    if (rel === 'stylesheet' || as === 'style') {
+      element = document.head.querySelector(`link[href="${href}"]`) || document.createElement('link');
+      element.setAttribute('rel', rel);
+      if (as) element.setAttribute('as', as);
+      if (crossorigin) element.setAttribute('crossorigin', crossorigin);
+      if (fetchpriority) element.setAttribute('fetchpriority', fetchpriority);
+      element.setAttribute('href', href);
+      element.onload = () => {
+        if (callback) callback('load');
+        resolve('CSS loaded');
+      };
+      element.onerror = () => {
+        if (callback) callback('error');
+        reject(new Error(`Failed to load CSS: ${href}`));
+      };
+      document.head.appendChild(element);
+    } else if (rel === 'script' || as === 'script') {
+      element = document.body.querySelector(`script[src="${href}"]`) || document.createElement('script');
+      element.src = href;
+      element.async = true;
+      element.type = 'module';
+      element.onload = () => {
+        if (callback) callback('load');
+        resolve('JS loaded');
+      };
+      element.onerror = () => {
+        if (callback) callback('error');
+        reject(new Error(`Failed to load JS: ${href}`));
+      };
+      document.body.appendChild(element);
+    } else {
+      reject(new Error('Unsupported resource type'));
+    }
+  });
+}
+
 export async function priorityLoad(parr) {
   const promiseArr = [];
   parr.forEach((p) => {
     if (p.endsWith('.js')) {
-      const pr = new Promise((res) => { loadLink(p, { as: 'script', rel: 'modulepreload', callback: res }); });
+      const pr = loadLinks(p, { as: 'script', rel: 'modulepreload' });
       promiseArr.push(pr);
     } else if (p.endsWith('.css')) {
-      const pr = new Promise((res) => { loadLink(p, { rel: 'stylesheet', callback: res }); });
+      const pr = loadLinks(p, { rel: 'stylesheet' });
       promiseArr.push(pr);
     } else {
       promiseArr.push(fetch(p));
