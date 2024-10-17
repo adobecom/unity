745712e96b2905bb7bf7f988f31dc73a356767d8
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-10-17 14:46:46 -0700

    Update action-binder.js

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -321,7 +321,7 @@ export default class ActionBinder {
       };
       const finalizeJson = await this.serviceHandler.postCallToService(
         this.acrobatApiConfig.acrobatEndpoint.finalizeAsset,
-        { body: JSON.stringify(finalAssetData), signal: AbortSignal.timeout ? AbortSignal.timeout(15000) : undefined },
+        { body: JSON.stringify(finalAssetData) },
       );
       if (!finalizeJson || Object.keys(finalizeJson).length !== 0) {
         await this.showSplashScreen();
