7d24234e5529c40e3e027a7c200a102d7ea7150b
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-11-26 11:58:27 -0800

    add error code

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -156,9 +156,10 @@ export default class ActionBinder {
 
   async dispatchErrorToast(code, status, info = null, showError = true) {
     if (showError) {
-      const message = code in this.workflowCfg.errors
+      const errorMessage = code in this.workflowCfg.errors
         ? this.workflowCfg.errors[code]
         : await getError(this.workflowCfg.enabledFeatures[0], code);
+      const message = code.includes('cookie_not_set') ? '' : errorMessage || 'Unable to process the request';
       this.block.dispatchEvent(new CustomEvent(
         unityConfig.errorToastEvent,
         {
