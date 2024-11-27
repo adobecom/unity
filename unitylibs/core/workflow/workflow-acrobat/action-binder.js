92832eac0a461068eebf96de0bf7555dbd0737fb
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-11-26 16:09:33 -0800

    Update action-binder.js

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -168,7 +168,7 @@ export default class ActionBinder {
         {
           detail: {
             code,
-            message: `${message || 'Unable to process the request'}`,
+            message: `${message}`,
             status,
             info,
             accountType: this.getAccountType(),
