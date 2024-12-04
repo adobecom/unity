d810e888da003a8f1c8684610769397ff12b24ad
mathuria <mathuria@adobe.com>
2024-12-04 11:39:53 +0530

    Unitytest

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -30,10 +30,10 @@ export function decorateArea(area = document) {}
 const miloLibs = setLibs('/libs');
 
 const {
-  createTag, getConfig, loadStyle, loadLink, loadScript, localizeLink, loadArea,
+  createTag, getConfig, loadStyle, loadLink, loadScript, localizeLink, loadArea, decorateDefaultLinkAnalytics
 } = await import(`${miloLibs}/utils/utils.js`);
 export {
-  createTag, loadStyle, getConfig, loadLink, loadScript, localizeLink, loadArea,
+  createTag, loadStyle, getConfig, loadLink, loadScript, localizeLink, loadArea, decorateDefaultLinkAnalytics
 };
 
 export function getGuestAccessToken() {
