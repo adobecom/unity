34bba22a8f6e880d4630737e236dd4ef0eb15b4c
Hariprasad N V <hnv@adobe.com>
2025-01-02 11:08:43 +0530

    Text to image

--- unitylibs/core/workflow/workflow-genai/widget.js
+++ unitylibs/core/workflow/workflow-genai/widget.js
@@ -18,27 +18,66 @@ export default class UnityWidget {
       ariaOwns: 'dropdown',
       ariaHaspopup: 'listbox',
     });
-
-    Object.keys(this.workflowCfg.targetCfg.actionMap).forEach((cls, idx) => {
-      const el = createTag(idx === 0 ? 'input' : 'a', {
-        class: cls.replace('.', ''),
-        ...(idx === 0 ? { type: 'text' } : { href: '#' }),
-        ...(idx !== 0 ? this.workflowCfg.supportedTexts[cls.replace('.', '').split('-')[0]]?.innerText : '')
-      });
-      con.append(el);
+    const inpText = createTag('input', {
+      class: 'input-class',
+      type: 'text',
+      placeholder: 'Something for placeholder',
     });
+    const surBtn = this.createActionBtn(
+      this.el.querySelector('icon-surpriseMe')?.closest('li'),
+      'surprise-btn-class'
+    );
+    const genBtn = this.createActionBtn(
+      this.el.querySelector('icon-generate')?.closest('li'),
+      'generate-btn-class'
+    );
+    const dropCon = this.createDropdown();
+    con.append(inpText, surBtn, genBtn, dropCon);
+    this.widget.append(con);
+    this.target.append(this.widget);
+    return this.actionMap;
+  }
+
+  createDropdown() {
     const dropCon = createTag('ul', {
       class: 'dropdown hidden',
       role: 'listbox',
       ariaLabelledby: 'promptInput',
     });
-    con.append(dropCon);
-    this.widget.append(con);
-    const interactiveArea = this.target.querySelector(
-      'div[data-valign="middle"].text'
-    );
-    const Paragraphs = interactiveArea.querySelectorAll('p.body-m');
-    interactiveArea.insertBefore(this.widget, Paragraphs[1]);
-    return this.actionMap;
+    const promptTitle = createTag('li', { class: 'dropdown-title', role: 'presentation' }, 'Prompt Suggestions');
+    dropCon.append(promptTitle);
+    if (this.workflowCfg?.supportedTexts?.prompt?.array) {
+      this.workflowCfg.supportedTexts.prompt.array.forEach((text) => {
+        const prompt = createTag('li', { class: 'dropdown-item', role: 'option' }, text);
+        dropCon.append(prompt);
+      });
+    }
+    const separator = createTag('li', { class: 'dropdown-separator', role: 'separator' });
+    dropCon.append(separator);
+
+    const footer = createTag('li', { class: 'dropdown-footer' });
+    const tipText = createTag('span', { class: 'tip-text' }, this.workflowCfg.supportedTexts.tip.array[0].innerText);
+    const legalText = createTag('span', { class: 'tip-text' }, this.workflowCfg.supportedTexts.tip.array[0].querySelector('a'));
+    footer.append(tipText);
+    footer.append(legalText);
+    dropCon.append(footer);
+    return dropCon;
+  }
+
+  createActionBtn(btnCfg, btnClass) {
+    if (!btnCfg) return null;
+    const txt = btnCfg.innerText?.trim();
+    const img = btnCfg.querySelector('img[src*=".svg"]');
+    const actionBtn = createTag('a', { href: '#', class: `unity-action-btn ${btnClass}` });
+    let swapOrder = false;
+    if (img) {
+      actionBtn.append(createTag('div', { class: 'btn-icon' }, img));
+      if (img.nextSibling?.nodeName === '#text') swapOrder = true;
+    }
+    if (txt) {
+      const btnTxt = createTag('div', { class: 'btn-text' }, txt.split('\n')[0]);
+      swapOrder ? actionBtn.prepend(btnTxt) : actionBtn.append(btnTxt);
+    }
+    return actionBtn;
   }
 }
