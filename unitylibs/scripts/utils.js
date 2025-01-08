fd4123842833deb724e8f48c66be42f0e44acc0d
Hariprasad N V <hnv@adobe.com>
2025-01-08 13:52:07 +0530

    change the intersection

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -205,14 +205,33 @@ export async function retryRequestUntilProductRedirect(cfg, requestFunction, del
   return cfg.scanResponseAfterRetries;
 }
 
+// export function createIntersectionObserver({ el, callback, cfg, options = {} }) {
+//   const io = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       if (!entry.isIntersecting) {
+//         callback(cfg);
+//       }
+//     });
+//   }, options);
+//   io.observe(el);
+//   return io;
+// }
+
 export function createIntersectionObserver({ el, callback, cfg, options = {} }) {
   const io = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (!entry.isIntersecting) {
+        console.log('entry', entry.isVisible);
+        cfg.isIntersecting = false;
+        callback(cfg);
+      } else if (entry.isIntersecting && cfg?.stickyBehavior) {
+        console.log('entry1', entry.isVisible);
+        cfg.isIntersecting = true;
         callback(cfg);
       }
     });
   }, options);
+
   io.observe(el);
   return io;
 }
