73e590ddd25c09e574a8f0914af622ab1f950def
Hariprasad N V <hnv@adobe.com>
2025-01-13 16:14:04 +0530

    test changes

--- unitylibs/core/workflow/workflow-genai/widget.js
+++ unitylibs/core/workflow/workflow-genai/widget.js
@@ -153,7 +153,7 @@ export default class UnityWidget {
       cfg: this.workflowCfg,
       options: {
         root: null,
-        rootMargin: '100px',
+        rootMargin: '10px',
         threshold: 0.1,
       },
     });
