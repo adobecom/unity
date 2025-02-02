aa373ac16dca32bf8555e028b6435b55c071d327
Hariprasad N V <hnv@adobe.com>
2025-02-02 15:38:52 +0530

    latest changes

--- unitylibs/core/workflow/workflow-ai/widget.js
+++ unitylibs/core/workflow/workflow-ai/widget.js
@@ -67,7 +67,7 @@ export default class UnityWidget {
       'aria-labelledby': 'promptInput',
       'aria-hidden': 'true',
     });
-    const titleCon = createTag('li', { id: 'tip-content', class: 'drop-title-con', 'aria-labelledby': 'prompt-suggestions', tabindex: '-1' });
+    const titleCon = createTag('li', { class: 'drop-title-con', 'aria-labelledby': 'prompt-suggestions' });
     const title = createTag('span', { class: 'drop-title', id: 'prompt-suggestions' }, `${ph['placeholder-prompt']} ${ph['placeholder-suggestions']}`);
     const closeBtn = createTag('button', { class: 'close-btn', 'daa-ll': 'drop-close', 'aria-label': 'Close dropdown' });
     titleCon.append(title, closeBtn);
@@ -93,7 +93,7 @@ export default class UnityWidget {
   createFooter(ph) {
     const footer = createTag('li', { class: 'drop-footer' });
     const tipEl = this.el.querySelector('.icon-tip')?.closest('li');
-    const tipCon = createTag('div', { class: 'tip-con', role: 'note', 'aria-label': `${ph['placeholder-tip']} ${tipEl?.innerText}` });
+    const tipCon = createTag('div', { id: 'tip-content', class: 'tip-con', tabindex: '-1', role: 'note', 'aria-label': `${ph['placeholder-tip']} ${tipEl?.innerText}` });
     const tipText = createTag('span', { class: 'tip-text', id: 'tip-text' }, `${ph['placeholder-tip']}:`);
     const tipDesc = createTag('span', { class: 'tip-desc', id: 'tip-desc' }, tipEl?.innerText || '');
     tipCon.append(tipText, tipDesc);
