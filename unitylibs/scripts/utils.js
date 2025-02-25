12f323ad4279ef9482ccfff592b5c5b5201a4403
mathuria <mathuria@adobe.com>
2025-02-25 12:48:05 +0530

    Remove v1 code

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -67,33 +67,6 @@ export function getLocale() {
   return currLocale ? currLocale : 'us';
 }
 
-export async function loadSvg(src) {
-  try {
-    const res = await fetch(src, { mode: 'no-cors' });
-    if (!res.status === 200) return null;
-    const svg = await res.text();
-    return svg;
-  } catch (e) {
-    return '';
-  }
-}
-
-export async function loadSvgs(svgs) {
-  const promiseArr = [];
-  [...svgs].forEach((svg) => {
-    promiseArr.push(
-      fetch(svg.src)
-        .then((res) => {
-          if (res.ok) return res.text();
-          else throw new Error('Could not fetch SVG');
-        })
-        .then((txt) => { svg.parentElement.innerHTML = txt; })
-        .catch((e) => { svg.remove(); }),
-    );
-  });
-  await Promise.all(promiseArr);
-}
-
 export function loadImg(img) {
   return new Promise((res) => {
     img.loading = 'eager';
@@ -104,27 +77,6 @@ export function loadImg(img) {
       img.onerror = () => res();
     }
   });
-}
-
-export async function createActionBtn(btnCfg, btnClass, iconAsImg = false, swapOrder = false) {
-  const txt = btnCfg.innerText;
-  const img = btnCfg.querySelector('img[src*=".svg"]');
-  const actionBtn = createTag('a', { href: '#', class: `unity-action-btn ${btnClass}` });
-  if (img) {
-    let btnImg = null;
-    const { pathname } = new URL(img.src);
-    const libSrcPath = `${getUnityLibs().split('/unitylibs')[0]}${pathname}`;
-    if (iconAsImg) btnImg = createTag('img', { src: libSrcPath });
-    else btnImg = await loadSvg(libSrcPath);
-    const btnIcon = createTag('div', { class: 'btn-icon' }, btnImg);
-    actionBtn.append(btnIcon);
-  }
-  if (txt) {
-    const btnTxt = createTag('div', { class: 'btn-text' }, txt.split('\n')[0].trim());
-    if (swapOrder) actionBtn.prepend(btnTxt);
-    else actionBtn.append(btnTxt);
-  }
-  return actionBtn;
 }
 
 export async function priorityLoad(parr) {
@@ -143,76 +95,6 @@ export async function priorityLoad(parr) {
   return await Promise.all(promiseArr);
 }
 
-async function createErrorToast() {
-  const [alertImg, closeImg] = await Promise.all([
-    fetch(`${getUnityLibs()}/img/icons/alert.svg`).then((res) => res.text()),
-    fetch(`${getUnityLibs()}/img/icons/close.svg`).then((res) => res.text()),
-  ]);
-  const errholder = createTag('div', { class: 'alert-holder' });
-  const errdom = createTag('div', { class: 'alert-toast' });
-  const alertContent = createTag('div', { class: 'alert-content' });
-  const alertIcon = createTag('div', { class: 'alert-icon' });
-  const alertText = createTag('div', { class: 'alert-text' });
-  const p = createTag('p', {}, 'Alert Text');
-  alertText.append(p);
-  alertIcon.innerHTML = alertImg;
-  alertIcon.append(alertText);
-  const alertClose = createTag('a', { class: 'alert-close', href: '#' });
-  const alertCloseText = createTag('span', { class: 'alert-close-text' }, 'Close error toast');
-  alertClose.innerHTML = closeImg;
-  alertClose.append(alertCloseText);
-  alertContent.append(alertIcon, alertClose);
-  errdom.append(alertContent);
-  errholder.append(errdom);
-  alertClose.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.target.closest('.alert-holder').style.display = 'none';
-  });
-  const { decorateDefaultLinkAnalytics } = await import(`${miloLibs}/martech/attributes.js`);
-  decorateDefaultLinkAnalytics(errholder);
-  return errholder;
-}
-
-export async function showErrorToast(targetEl, unityEl, className) {
-  const alertHolder = targetEl.querySelector('.alert-holder');
-  if (!alertHolder) {
-    const errorToast = await createErrorToast();
-    targetEl.append(errorToast);
-  }
-  const msg = unityEl.querySelector(className)?.nextSibling.textContent;
-  document.querySelector('.unity-enabled .interactive-area .alert-holder .alert-toast .alert-text p').innerText = msg;
-  document.querySelector('.unity-enabled .interactive-area .alert-holder').style.display = 'flex';
-}
-
-export async function retryRequestUntilProductRedirect(cfg, requestFunction, delay = 1000) {
-  while (cfg.continueRetrying) {
-    try {
-      const scanResponse = await requestFunction();
-      if (scanResponse.status === 429 || (scanResponse.status >= 500 && scanResponse.status < 600)) {
-        await new Promise((res) => setTimeout(res, delay));
-      } else {
-        cfg.scanResponseAfterRetries = scanResponse;
-        return scanResponse;
-      }
-    } catch (e) {
-      await new Promise((res) => setTimeout(res, delay));
-    }
-  }
-  return cfg.scanResponseAfterRetries;
-}
-
-export function createIntersectionObserver({ el, callback, cfg, options = {} }) {
-  const io = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        callback(cfg);
-      }
-    });
-  }, options);
-  io.observe(el);
-  return io;
-}
-
 export function delay(durationMs = 1000) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -228,7 +110,6 @@ export function updateQueryParameter(url, paramName='format', oldValue='webply',
       if (params.get(paramName) === oldValue) {
           params.set(paramName, newValue);
       }
-
       return urlObj.toString();
   } catch (error) {
       return null;
