eb79cbf59b6c6bbb24e866f7ed4ed42deb1bcb5e
arugupta1992 <gupta.arushi92@gmail.com>
2025-03-19 16:40:22 +0530

    Defining DOS_SPECIAL_NAMES

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -14,6 +14,11 @@ import {
   loadImg,
 } from '../../../scripts/utils.js';
 
+const DOS_SPECIAL_NAMES = [
+  'CON', 'PRN', 'AUX', 'NUL', 'COM0', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6',
+  'COM7', 'COM8', 'COM9', 'LPT0', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6',
+  'LPT7', 'LPT8', 'LPT9'];
+
 class ServiceHandler {
   getGuestAccessToken() {
     try {
