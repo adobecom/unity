b5dfc4ccf26c4afede9d66bd9aaddd391b9c0976
arugupta1992 <gupta.arushi92@gmail.com>
2025-03-21 15:15:08 +0530

    Addressing PR comments + adding exception handling and further optimisation in sanitizeFileName

--- unitylibs/utils/StringUtils.js
+++ unitylibs/utils/StringUtils.js
@@ -1,23 +1,7 @@
-/**
-   * Returns the extension of a file name, if any.
-   * @param {string} name - Target file name.
-   * @returns {string} Extension or an empty string if none exists.
-   */
 export function getExtension(name) {
-    if (name) {
-      const segments = name.split('.');
-      if (segments.length > 1) {
-        return segments.pop(); // Get the last segment as the extension
-      }
-    }
-    return '';
+    return name && name.includes('.') ? name.split('.').pop() : '';
   }
 
-  /**
-   * Returns the file name without its extension.
-   * @param {string} name - Target file name.
-   * @returns {string} Name without extension.
-   */
 export function withoutExtension(name) {
     if (name) {
       const lastDot = name.lastIndexOf('.');
@@ -25,5 +9,5 @@ export function withoutExtension(name) {
         return name.substring(0, lastDot);
       }
     }
-    return name; // Return original name if no extension is found
+    return name;
   }
