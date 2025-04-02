779bb69bf0bc37deec5900f67cc00ee2de430967
Arushi Gupta <65466846+arugupta1992@users.noreply.github.com>
2025-04-02 20:05:43 +0530

    Move the logic to send userType in redirect url to backend  (#329)
    Resolves: [MWPW-170102](https://jira.corp.adobe.com/browse/MWPW-170102)
    
    Test URLs:
    
    Before:
    https://stage--dc--adobecom.hlx.page/acrobat/online/compress-pdf?unitylibs=stage
    After:
    https://stage--dc--adobecom.hlx.page/acrobat/online/compress-pdf?unitylibs=arugupta-170102

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -319,10 +319,7 @@ export default class ActionBinder {
       .then(async (resArr) => {
         const response = resArr[resArr.length - 1];
         if (!response?.url) throw new Error('Error connecting to App');
-        const userType = localStorage.getItem('unity.user') === 'true' ? 'frictionless_return_user' : 'frictionless_new_user';
-        const url = new URL(response.url);
-        url.searchParams.append('user', userType);
-        this.redirectUrl = url.href;
+        this.redirectUrl = response.url;
       })
       .catch(async (e) => {
         const { default: TransitionScreen } = await import(`${getUnityLibs()}/scripts/transition-screen.js`);
