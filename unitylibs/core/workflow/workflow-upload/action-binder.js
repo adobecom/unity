2780a9e57c2652a4817def0cb03f8931ec9f4476
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2025-04-08 18:11:09 -0700

    Update action-binder.js

--- unitylibs/core/workflow/workflow-upload/action-binder.js
+++ unitylibs/core/workflow/workflow-upload/action-binder.js
@@ -216,7 +216,7 @@ export default class ActionBinder {
       },
     };
     try {
-      await delay(2000);
+      await this.delay(2000);
       const { default: TransitionScreen } = await import(`${getUnityLibs()}/scripts/transition-screen.js`);
       this.LOADER_LIMIT = 100;
       this.transitionScreen = new TransitionScreen(this.transitionScreen.splashScreenEl, this.initActionListeners, this.LOADER_LIMIT, this.workflowCfg);
