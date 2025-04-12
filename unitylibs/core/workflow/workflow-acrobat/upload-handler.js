9bdf66775b71309eb94f3cdad2ece3e0c1846e31
arugupta1992 <gupta.arushi92@gmail.com>
2025-04-12 22:39:19 +0530

    sample error payload for testing

--- unitylibs/core/workflow/workflow-acrobat/upload-handler.js
+++ unitylibs/core/workflow/workflow-acrobat/upload-handler.js
@@ -324,7 +324,8 @@ export default class UploadHandler {
     };
     const redirectSuccess = await this.actionBinder.handleRedirect(cOpts, fileData);
     if (!redirectSuccess) return;
-    this.actionBinder.dispatchAnalyticsEvent('uploading', fileData);
+    const errorData = {code: '-401', subCode: '106', desc: "Sample test message"}
+    this.actionBinder.dispatchAnalyticsEvent('uploading', {...fileData, errorData});
     const uploadResult = await this.chunkPdf(
       [assetData],
       [blobData],
