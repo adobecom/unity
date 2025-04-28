f32eeb6e8b81a17aad48becf9b05854a7e5627b8
rohitsahu <sahurohit9@gmail.com>
2025-04-29 00:36:10 +0530

    minor

--- unitylibs/core/workflow/workflow-acrobat/upload-handler.js
+++ unitylibs/core/workflow/workflow-acrobat/upload-handler.js
@@ -57,7 +57,7 @@ export default class UploadHandler {
         const response = await this.uploadFileToUnity(url, blobData, fileType, assetId, chunkNumber);
         if (response.ok) {
           this.actionBinder.dispatchAnalyticsEvent('chunk_uploaded', {
-            chunkUploadAttempt: `${attempt}`,
+            chunkUploadAttempt: attempt,
             assetId,
             chunkNumber,
             size: `${blobData.size}`,
@@ -442,7 +442,7 @@ export default class UploadHandler {
       const validated = await this.handleValidations(assetData);
       if (!validated) return;
     }
-    this.actionBinder.dispatchAnalyticsEvent('uploaded', { ...fileData, assetId: assetData.id, chunkUploadAttempt: `${attemptMap.get(0)}` });
+    this.actionBinder.dispatchAnalyticsEvent('uploaded', { ...fileData, assetId: assetData.id, maxRetryCount: attemptMap.get(0) });
   }
 
   async singleFileGuestUpload(file, fileData) {
