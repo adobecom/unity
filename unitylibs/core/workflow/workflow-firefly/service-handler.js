24fc8b311fc2ee13c3dbc2973edea694b8f18cb5
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2025-05-08 13:51:36 -0700

    firefly

---
+++ unitylibs/core/workflow/workflow-firefly/service-handler.js
@@ -0,0 +1,39 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-restricted-syntax */
+
+import { unityConfig, getHeaders } from '../../../scripts/utils.js';
+
+export default class ServiceHandler {
+  constructor(renderWidget = false, canvasArea = null) {
+    this.renderWidget = renderWidget;
+    this.canvasArea = canvasArea;
+  }
+
+  async fetchFromService(url, options) {
+    try {
+      const response = await fetch(url, options);
+      const error = new Error();
+      if (response.status !== 200) {
+        error.status = response.status;
+        throw error;
+      }
+      return response.json();
+    } catch (error) {
+      if (error.name === 'TimeoutError' || error.name === 'AbortError') {
+        error.status = 504;
+      }
+      throw error;
+    }
+  }
+
+  async postCallToService(api, options) {
+    const postOpts = {
+      method: 'POST',
+      headers: await getHeaders(unityConfig.apiKey),
+      ...options,
+    };
+    return this.fetchFromService(api, postOpts);
+  }
+}
