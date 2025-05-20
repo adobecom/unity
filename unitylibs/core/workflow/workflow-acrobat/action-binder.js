21f21cc216615fc22a7452707d9660cf01b47bd8
arugupta1992 <gupta.arushi92@gmail.com>
2025-05-20 11:21:03 +0530

    Removing comments

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -488,9 +488,7 @@ static ERROR_MAP = {
   }
 
   async handleSingleFileUpload(files) {
-    //this.filesData = { type: files[0].type, size: files[0].size, count: 1, uploadType: 'sfu' };
     this.filesData = {...this.filesData,uploadType: 'sfu'};
-    //this.dispatchAnalyticsEvent(eventName, this.filesData);
     if (this.signedOut) await this.uploadHandler.singleFileGuestUpload(files[0], this.filesData);
     else await this.uploadHandler.singleFileUserUpload(files[0], this.filesData);
   }
@@ -498,10 +496,7 @@ static ERROR_MAP = {
   async handleMultiFileUpload(files) {
     this.MULTI_FILE = true;
     this.LOADER_LIMIT = 65;
-    //const isMixedFileTypes = this.isMixedFileTypes(files);
-    // this.filesData = { type: isMixedFileTypes, size: totalFileSize, count: files.length, uploadType: 'mfu' };
     this.filesData = {...this.filesData,uploadType: 'mfu'};
-    //this.dispatchAnalyticsEvent(eventName, this.filesData);
     this.dispatchAnalyticsEvent('multifile', this.filesData);
     if (this.signedOut) await this.uploadHandler.multiFileGuestUpload(files, this.filesData);
     else await this.uploadHandler.multiFileUserUpload(files, this.filesData);
