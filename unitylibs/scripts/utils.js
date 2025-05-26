8129ce389d3770d9c9ed01d9ec1cf8dbaf11e4e3
Rohit Sahu <rosahu@adobe.com>
2025-05-26 16:46:20 +0530

    minor fix

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -205,12 +205,12 @@ export async function priorityLoad(parr, additionalParams = '') {
   parr.forEach((p) => {
     if (p.endsWith('.js')) {
       const pr = loadScript(p, 'module', { mode: 'async' });
-      promiseArr.push(pr+additionalParams);
+      promiseArr.push(pr);
     } else if (p.endsWith('.css')) {
       const pr = new Promise((res) => { loadLink(p, { rel: 'stylesheet', callback: res }); });
-      promiseArr.push(pr+additionalParams);
+      promiseArr.push(pr);
     } else {
-      promiseArr.push(fetch(p)+additionalParams);
+      promiseArr.push(fetch(p+additionalParams));
     }
   });
   return await Promise.all(promiseArr);
