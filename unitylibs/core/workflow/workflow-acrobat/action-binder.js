e5a6b7650c15d084549a4be8c8d0672434a0e555
Rohit Sahu <rosahu@adobe.com>
2025-05-27 15:33:33 +0530

    set additional query params as soon as we fetch the redirect url

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -525,6 +525,9 @@ export default class ActionBinder {
     }
     await this.getRedirectUrl(cOpts);
     if (!this.redirectUrl) return false;
+    const [baseUrl, queryString] = this.redirectUrl.split('?');
+    const additionalParams = unityConfig.env === 'stage' ? `${window.location.search.slice(1)}&` : '';
+    this.redirectUrl = `${baseUrl}?${additionalParams}&${queryString}`;
     this.dispatchAnalyticsEvent('redirectUrl', {...filesData, redirectUrl: this.redirectUrl});
     return true;
   }
@@ -619,11 +622,9 @@ export default class ActionBinder {
     this.transitionScreen.updateProgressBar(this.transitionScreen.splashScreenEl, 100);
     try {
       await this.delay(500);
-      const [baseUrl, queryString] = this.redirectUrl.split('?');
-      const additionalParams = unityConfig.env === 'stage' ? `${window.location.search.slice(1)}&` : '';
       if (this.multiFileFailure && this.redirectUrl.includes('#folder')) {
-        window.location.href = `${baseUrl}?${additionalParams}feedback=${this.multiFileFailure}&${queryString}`;
-      } else window.location.href = `${baseUrl}?${this.redirectWithoutUpload === false ? `UTS_Uploaded=${this.uploadTimestamp}&` : ''}${additionalParams}${queryString}`;
+        window.location.href = `${this.redirectUrl}&feedback=${this.multiFileFailure}`;
+      } else window.location.href = `${this.redirectUrl}?${this.redirectWithoutUpload === false ? `&UTS_Uploaded=${this.uploadTimestamp}` : ''}`;
     } catch (e) {
       await this.transitionScreen.showSplashScreen();
       await this.dispatchErrorToast('error_generic', 500, `Exception thrown when redirecting to product; ${e.message}`, false, e.showError, {
