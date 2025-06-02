04a28963e6b5ffd3366b52b566709412c1b88919
Rohit Sahu <rosahu@adobe.com>
2025-06-02 12:01:38 +0530

    fix the verb name in isSameFileType method after the verb name was changed to  pdf-to-image from pdf-to-jpf/png
    (cherry picked from commit abeb70a60561cce38f8c1fb7607b34b05b423ebf)

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -418,8 +418,7 @@ export default class ActionBinder {
       'pdf-to-word': ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msword', 'application/rtf'],
       'pdf-to-excel': ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
       'pdf-to-ppt': ['application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation'],
-      'pdf-to-jpg': ['image/jpeg'],
-      'pdf-to-png': ['image/png'],
+      'pdf-to-image': ['image/jpeg', 'image/png'],
     };
     return verbToFileTypeMap[verb]?.includes(fileType) || false;
   }
