f418f66a5ba5397ef5f7afd81761f441e76bff71
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2025-06-24 17:31:20 -0700

    update the commit

---
+++ nala/features/png-pdf/png-to-pdf.spec.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  FeatureName: 'PNG to PDF',
+  features: [
+    {
+      tcid: '0',
+      name: '@png-pdf',
+      path: '/drafts/nala/acrobat/online/test/png-to-pdf',
+      data: {
+        verbTitle: 'Adobe Acrobat',
+        verbHeading: 'PNG to PDF converter',
+        verbCopy: 'Drag and drop a PNG image file to use our PDF converter.',
+      },
+      tags: '@png-to-pdf @smoke @regression @unity',
+    },
+  ],
+};
