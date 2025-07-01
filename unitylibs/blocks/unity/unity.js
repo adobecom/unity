4519e4bc592f1503d7f615065290eec7477e8425
Rohit Sahu <rosahu@adobe.com>
2025-07-01 16:25:41 +0530

    add alloy call to fetch experiment inside the init code

--- unitylibs/blocks/unity/unity.js
+++ unitylibs/blocks/unity/unity.js
@@ -10,9 +10,29 @@ function getUnityLibs(prodLibs, project = 'unity') {
   }
   const branch = new URLSearchParams(window.location.search).get('unitylibs') || 'main';
   const helixVersion = hostname.includes('.hlx.') ? 'hlx' : 'aem';
-  return branch.indexOf('--') > -1 
-  ? `https://${branch}.${helixVersion}.live/unitylibs` 
-  : `https://${branch}--unity--adobecom.${helixVersion}.live/unitylibs`;
+  return branch.indexOf('--') > -1
+    ? `https://${branch}.${helixVersion}.live/unitylibs`
+    : `https://${branch}--unity--adobecom.${helixVersion}.live/unitylibs`;
+}
+
+function handlePropositions(AJOPropositionResult) {
+  console.log('AJOPropositionResult: ', AJOPropositionResult);
+}
+
+function fetchAjoResponse() {
+  window._satellite.track('propositionFetch', {
+    personalization: { surfaces: ['web://adobe.com/acrobat#projectUnity'] },
+    data: {},
+    xdm: {},
+    done(AJOPropositionResult, error) {
+      if (error) {
+        console.error('[AJO Fetch Error]:', err);
+        return;
+      }
+      handlePropositions(AJOPropositionResult);
+      window._satellite.track('propositionDisplay', AJOPropositionResult.propositions); // update Analytics/CJA response was displayed
+    },
+  });
 }
 
 export default async function init(el) {
@@ -23,5 +43,6 @@ export default async function init(el) {
   });
   await stylePromise;
   const { default: wfinit } = await import(`${unitylibs}/core/workflow/workflow.js`);
+  fetchAjoResponse();
   await wfinit(el, projectName, unitylibs, 'v2');
 }
