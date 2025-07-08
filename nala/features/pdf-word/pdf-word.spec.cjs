d81920ce616073824ae910809cf9ae4e5e80397c
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2025-07-08 10:29:17 -0700

    [NALA]UI Automation tests scripts for batch 2A verbs (#491)
    <!-- Before submitting, please review all open PRs. -->
    
    PR includes nala ui automation test scripts for following `Batch 2A`
    verbs
    
    - pdf-to-word
    - pdf-to-excel
    - pdf-to-ppt
    - pdf-to-jpg
    - word-to-pdf
    - excel-to-pdf
    - ppt-to-pdf
    - jpg-to-pdf
    - png-to-pdf
    
    Resolves following jira items
    - [MWPW-175417](https://jira.corp.adobe.com/browse/MWPW-175417)
    - [MWPW-175419](https://jira.corp.adobe.com/browse/MWPW-175419)
    - [MWPW-175420](https://jira.corp.adobe.com/browse/MWPW-175420)
    - [MWPW-175422](https://jira.corp.adobe.com/browse/MWPW-175422)
    - [MWPW-175423](https://jira.corp.adobe.com/browse/MWPW-175423)
    - [MWPW-175424](https://jira.corp.adobe.com/browse/MWPW-175424)
    - [MWPW-175425](https://jira.corp.adobe.com/browse/MWPW-175425)
    - [MWPW-175426](https://jira.corp.adobe.com/browse/MWPW-175426)
    - [MWPW-175428](https://jira.corp.adobe.com/browse/MWPW-175428)
    
    **Test URLs:**
    - Before: https://main--unity--adobecom.aem.page/?martech=off
    - After: https://nala-batch2a--unity--adobecom.aem.page/?martech=off

---
+++ nala/features/pdf-word/pdf-word.spec.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  FeatureName: 'Pdf to Word',
+  features: [
+    {
+      tcid: '0',
+      name: '@pdf-word',
+      path: '/drafts/nala/acrobat/online/test/pdf-to-word',
+      data: {
+        verbTitle: 'Adobe Acrobat',
+        verbHeading: 'Convert PDF to Word',
+        verbCopy: 'Drag and drop a PDF file to use our PDF to Microsoft Word converter.',
+      },
+      tags: '@pdf-to-word @smoke @regression @unity',
+    },
+  ],
+};
