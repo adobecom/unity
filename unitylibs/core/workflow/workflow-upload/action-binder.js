1c268e9d30e712dd8238168da0ca10ab42143959
Rohit Sahu <rosahu@adobe.com>
2025-08-07 18:10:11 +0530

    fixed for photoshop as well

--- unitylibs/core/workflow/workflow-upload/action-binder.js
+++ unitylibs/core/workflow/workflow-upload/action-binder.js
@@ -355,17 +355,21 @@ export default class ActionBinder {
     await this.continueInApp(this.assetId);
   }
 
-  async photoshopActionMaps(value, files) {
+  async loadTransitionScreen() {
     if (!this.transitionScreen) {
       try {
         const { default: TransitionScreen } = await import(`${getUnityLibs()}/scripts/transition-screen.js`);
         this.transitionScreen = new TransitionScreen(this.splashScreenEl, this.initActionListeners, this.LOADER_LIMIT, this.workflowCfg, this.desktop);
-        await this.transitionScreen.loadSplashFragment();
+        await this.transitionScreen.delayedSplashLoader();
       } catch (error) {
         window.lana?.log(`Message: Error loading transition screen, Error: ${error}`, this.lanaOptions);
+        throw error;
       }
     }
+  }
 
+  async photoshopActionMaps(value, files) {
+    await this.loadTransitionScreen();
     await this.handlePreloads();
     if (!this.errorToastEl) this.errorToastEl = await this.createErrorToast();
     switch (value) {
@@ -445,6 +449,9 @@ export default class ActionBinder {
         window.location.reload();
       }
     });
+    if (b === this.block) {
+      this.loadTransitionScreen();
+    }
   }
 
   preventDefault(e) {
