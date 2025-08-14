3ac832e5b6d49ebccaedd0532a43aec9b23cb5fb
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2025-08-14 16:27:32 -0700

    Update widget.js

--- unitylibs/core/workflow/workflow-photoshop/widget.js
+++ unitylibs/core/workflow/workflow-photoshop/widget.js
@@ -414,7 +414,7 @@ export default class UnityWidget {
       const { value } = actionSliderInput;
       const centerOffset = (value - minVal) / (maxVal - minVal);
       const isRtl = document.dir === 'rtl';
-      const moveCircle = isRtl ? 97 - (centerOffset * 94) : 3 + centerOffset * 94;
+      const moveCircle = isRtl ? centerOffset * 94 - 4 : 3 + centerOffset * 94;
       //const moveCircle = isRtl ? 97 - (centerOffset * 94) + 3 : 3 + centerOffset * 94;
       //const moveCircle = 3 + centerOffset * 94;
       actionSliderCircle.style[isRtl ? 'right' : 'left'] = `${moveCircle}%`;
