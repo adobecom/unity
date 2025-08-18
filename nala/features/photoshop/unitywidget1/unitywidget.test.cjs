8381a66942b6a25ddf329fa6ffc5af9265a42ed9
Sigadam Venkata Ramesh <venkatas@adobe.com>
2025-08-18 22:31:43 +0530

    PS automation tests
    PS automation tests

---
+++ nala/features/photoshop/unitywidget1/unitywidget.test.cjs
@@ -0,0 +1,72 @@
+import path from 'path';
+import { expect, test } from '@playwright/test';
+import { features } from './unitywidget.spac.cjs';
+import UnityWidget from './unitywidget.page.cjs';
+
+const imageFilePath = path.resolve(__dirname, '../../../assets/guddy.png');
+console.log(__dirname);
+
+let unityWidget;
+const unityLibs = process.env.UNITY_LIBS || '';
+
+test.describe('Unity Widget PS test suite', () => {
+  test.beforeEach(async ({ page }) => {
+    unityWidget = new UnityWidget(page);
+    await page.setViewportSize({ width: 1250, height: 850 });
+    await page.context().clearCookies();
+  });
+
+  // Test 0 : Unity Widget PS UI checks
+  test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
+    console.info(`[Test Page]: ${baseURL}${features[0].path}${unityLibs}`);
+
+    await test.step('step-1: Go to Unity Widget PS test page', async () => {
+      await page.goto(`${baseURL}${features[0].path}${unityLibs}`);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(`${baseURL}${features[0].path}${unityLibs}`);
+    });
+
+    await test.step('step-2: Verify Unity Widget PS verb user interface', async () => {
+      await page.waitForTimeout(3000);
+      await expect(await unityWidget.unityWidgetContainer).toBeTruthy();
+      await expect(await unityWidget.unityVideo).toBeTruthy();
+      await expect(await unityWidget.dropZone).toBeTruthy();
+      await expect(await unityWidget.dropZoneText).toBeTruthy();
+    });
+  });
+  // Test 1 : Unity Widget File Upload & splash screen display
+  test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
+    console.info(`[Test Page]: ${baseURL}${features[1].path}${unityLibs}`);
+
+    await test.step('check photoshop file upload', async () => {
+      await page.goto(`${baseURL}${features[1].path}${unityLibs}`);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(`${baseURL}${features[1].path}${unityLibs}`);
+    });
+    await test.step('png image file upload and splash screen display', async () => {
+      const fileInput = page.locator('//input[@type="file" and @id="file-upload"]').nth(0);
+      await page.waitForTimeout(10000);
+      await fileInput.setInputFiles(imageFilePath);
+      await page.waitForTimeout(3000);
+      await expect(unityWidget.splashScreen).toBeTruthy();
+    });
+  });
+  // Test 2 : Unity Widget user navigation to Photoshop Product Page
+  test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
+    console.info(`[Test Page]: ${baseURL}${features[2].path}${unityLibs}`);
+
+    await test.step('check user landing on PS product page post file upload', async () => {
+      await page.goto(`${baseURL}${features[2].path}${unityLibs}`);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(`${baseURL}${features[2].path}${unityLibs}`);
+    });
+    await test.step('png image file upload and user navigation to product page', async () => {
+      const fileInput = page.locator('//input[@type="file" and @id="file-upload"]').nth(0);
+      await page.waitForTimeout(10000);
+      await fileInput.setInputFiles(imageFilePath);
+      await page.waitForTimeout(10000);
+      const productPageUrl = await page.url();
+      expect(productPageUrl).toContain(features[2].url);
+    });
+  });
+});
