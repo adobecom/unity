e10d1b708ae3ccf64f417107f470b8f3683ee5a7
Sigadam Venkata Ramesh <venkatas@adobe.com>
2025-08-18 22:31:43 +0530

    PS automation tests
    PS automation tests

---
+++ nala/features/photoshop/unitywidget1/unitywidget.spac.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  FeatureName: 'PS Unity Widget',
+  features: [
+    {
+      tcid: '0',
+      name: '@ps-unityUI',
+      path: '/drafts/nala/unity/remove-background?georouting=off',
+      data: {
+        CTATxt: 'Upload your photo',
+        fileFormatTxt: 'File must be JPEG, JPG or PNG and up to 40MB',
+        dropZoneTxt: 'Drag and drop an image to try it today.',
+      },
+      tags: '@ps-unity @smoke @regression @unity @nopr',
+    },
+
+    {
+      tcid: '1',
+      name: '@ps-unityFileUpload',
+      path: '/drafts/nala/unity/remove-background?georouting=off',
+      tags: '@ps-unity @smoke @regression @unity @nopr',
+    },
+
+    {
+      tcid: '2',
+      name: '@ps-unityPSProductpage',
+      path: '/drafts/nala/unity/remove-background?georouting=off',
+      url: 'stage.try.photoshop.adobe.com',
+      tags: '@ps-unity @smoke @regression @unity @nopr',
+    },
+  ],
+};
