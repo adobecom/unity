c2aea6ccfd97b5cfa70f47713055aae1edda3793
vipulg <vipulg@adobe.com>
2025-09-06 18:20:15 +0530

    Revert "[MWPW-177382] Increase max concurrent files count to 10 for high end devices (#526)"
    This reverts commit b7e7f1058d356a2ac07619616559ab87bdb35467.

--- unitylibs/core/workflow/workflow-acrobat/upload-handler.js
+++ unitylibs/core/workflow/workflow-acrobat/upload-handler.js
@@ -10,9 +10,11 @@ export default class UploadHandler {
     this.serviceHandler = serviceHandler;
   }
 
-  getUploadLimits() {
-    return this.actionBinder.workflowCfg.targetCfg.uploadLimits;
-  }
+  static UPLOAD_LIMITS = {
+    HIGH_END: { files: 3, chunks: 10 },
+    MID_RANGE: { files: 3, chunks: 10 },
+    LOW_END: { files: 2, chunks: 6 },
+  };
 
   async createAsset(file, multifile = false, workflowId = null) {
     let assetData = null;
@@ -132,18 +134,16 @@ export default class UploadHandler {
     return 'MID_RANGE';
   }
 
-  async executeInBatches(items, maxConcurrent, processFn) {
+  async executeInBatches(items, batchSize, processFn) {
     const executing = new Set();
     for (const item of items) {
-      const promise = processFn(item)
-        .then(() => { executing.delete(promise); })
-        .catch(() => { executing.delete(promise); });
-      executing.add(promise);
-      if (executing.size >= maxConcurrent) await Promise.any(executing);
+      const p = processFn(item).then(() => executing.delete(p)).catch(() => {
+        executing.delete(p);
+      });
+      executing.add(p);
+      if (executing.size >= batchSize) await Promise.race(executing);
     }
-    if (executing.size > 0) {
-      await Promise.all(executing);
-    }
+    await Promise.all(executing);
   }
 
   async batchUpload(tasks, batchSize) {
@@ -332,13 +332,12 @@ export default class UploadHandler {
 
   getConcurrentLimits() {
     const deviceType = this.getDeviceType();
-    const limits = this.getUploadLimits();
     if (!this.actionBinder.MULTI_FILE) {
-      return { maxConcurrentChunks: limits[deviceType].chunks };
+      return { maxConcurrentChunks: UploadHandler.UPLOAD_LIMITS[deviceType].chunks };
     }
     return {
-      maxConcurrentFiles: limits[deviceType].files,
-      maxConcurrentChunks: limits[deviceType].chunks,
+      maxConcurrentFiles: UploadHandler.UPLOAD_LIMITS[deviceType].files,
+      maxConcurrentChunks: UploadHandler.UPLOAD_LIMITS[deviceType].chunks,
     };
   }
 
