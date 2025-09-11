0450e3ee3c6b1eaae89073582138b323559f89b7
vipulg <vipulg@adobe.com>
2025-09-11 16:58:16 +0530

    [NALA][MWPW-179389] Adding automation tests for PDF to PNG

---
+++ nala/features/pdf-png/pdf-to-png.spec.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  FeatureName: 'PDF to PNG',
+  features: [
+    {
+      tcid: '0',
+      name: '@pdf-png',
+      path: '/drafts/nala/acrobat/online/test/pdf-to-png',
+      data: {
+        verbTitle: 'Adobe Acrobat',
+        verbHeading: 'Convert a PDF to PNG image',
+        verbCopy: 'Drag and drop a PDF, then convert to JPG, PNG or TIFF file formats.',
+      },
+      tags: '@pdf-to-png @smoke @regression @unity',
+    },
+  ],
+};
