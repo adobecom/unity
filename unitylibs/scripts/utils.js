a3ec0e4f6dbee0c3ac2b17c7cc2ca8a0355e7538
vipulg <vipulg@adobe.com>
2025-10-14 12:22:59 +0530

    to be reverted - changing stage endpoint to dev

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -292,9 +292,10 @@ export const unityConfig = (() => {
       env: 'prod',
       ...commoncfg,
     },
+    // TODO: vipulg - change to stage.adobe.io
     stage: {
-      apiEndPoint: 'https://unity-stage.adobe.io/api/v1',
-      connectorApiEndPoint: 'https://unity-stage.adobe.io/api/v1/asset/connector',
+      apiEndPoint: 'https://unity-dev.adobe.io/api/v1',
+      connectorApiEndPoint: 'https://unity-dev.adobe.io/api/v1/asset/connector',
       env: 'stage',
       ...commoncfg,
     },
