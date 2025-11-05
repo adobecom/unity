de15499136293d6ce161bda1f950f66b47f355bc
dinesh-1763 <dineshkumar@adobe.com>
2025-11-05 20:58:17 +0530

    Ai Summary Generator Test Case

---
+++ nala/features/ai-summary-generator/ai-summary-generator.spec.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  FeatureName: 'AI Summary Generator',
+  features: [
+    {
+      tcid: '0',
+      name: '@ai-summary-generator',
+      path: '/drafts/nala/acrobat/online/test/ai-summary-generator',
+      data: {
+        verbTitle: 'Adobe Acrobat',
+        verbHeading: 'Summary generator',
+        verbCopy: 'Get fast, concise, and highly secure AI-generated summaries of documents — powered by Acrobat AI Assistant.',
+      },
+      tags: '@ai-summary-generator @smoke @regression @unity',
+    },
+  ],
+};
