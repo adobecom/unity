37e5bb6b6611496a1c4a8f4ff2904686f6baa228
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2026-01-23 10:48:50 -0800

    [MWPW-185735]Load milo libs from adobe.com for acrobat.adobe.com domain (#657)
    * Load milo libs from adobe.com for acrobat.adobe.com domain
    
    Resolves: [MWPW-185735](https://jira.corp.adobe.com/browse/MWPW-185735)
    
    **Test URLs:**
    Will be tested on stage.acrobat.adobe.com
    
    Note:
    This will need to be merged once akamai mapping is done on
    stage.acrobat.adobe for frictionless page.
    More details in the jira ticket
    **Update:** DC side PR is merged and we are getting ready to test akamai
    setting on acrobat.adobe.com side

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -4,6 +4,8 @@ export const [setLibs, getLibs] = (() => {
     (prodLibs, location) => {
       libs = (() => {
         const { hostname, search } = location || window.location;
+        if (hostname === 'acrobat.adobe.com') return 'https://www.adobe.com/libs';
+        if (hostname === 'stage.acrobat.adobe.com') return 'https://www.stage.adobe.com/libs';
         if (!(hostname.includes('.hlx.') || hostname.includes('.aem.') || hostname.includes('local'))) return prodLibs;
         const branch = new URLSearchParams(search).get('milolibs') || 'main';
         if (branch === 'local') return 'http://localhost:6456/libs';
@@ -305,6 +307,7 @@ export const unityConfig = (() => {
     || host.includes('aem.live')
     || host.includes('localhost')
     || host.includes('stage.adobe')
+    || host.includes('stage.acrobat.adobe')
     || host.includes('corp.adobe')
     || host.includes('graybox.adobe')) {
     return cfg.stage;
