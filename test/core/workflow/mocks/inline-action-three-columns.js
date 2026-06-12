d78f502cc87b6bfe4224fe23fd9a70f49a7c8cf8
Vipul Gupta <vipugpta@gmail.com>
2026-06-12 23:46:05 +0530

    Revert "Revert "[MWPW-194359] Remove Background Unity changes "" (#811)
    Reverts adobecom/unity#810
    
    And also fixes the crash issue in Acrobat workflow for the transition
    screen.
    
    ---------
    
    Co-authored-by: vipulg <vipulg@adobe.com>

---
+++ test/core/workflow/mocks/inline-action-three-columns.js
@@ -0,0 +1,25 @@
+export default `<div>
+  <div class="unity workflow-inline-action widget-inline-action">
+    <div>
+      <div>
+        <p><a href="/cc-shared/assets/svg/s2-icon-upload-20-n.svg">icon</a> Upload</p>
+        <p>Or tap here</p>
+        <p>Limits mobile</p>
+        <p>Legal</p>
+      </div>
+      <div>
+        <p><a href="/cc-shared/assets/svg/s2-icon-upload-20-n.svg">icon</a> Upload</p>
+        <p>Or tap here</p>
+        <p>Limits tablet</p>
+        <p>Legal</p>
+      </div>
+      <div>
+        <p><a href="/cc-shared/assets/svg/s2-icon-upload-20-n.svg">icon</a> Upload</p>
+        <p>Or drag and drop here</p>
+        <p>Limits desktop</p>
+        <p>Legal</p>
+      </div>
+    </div>
+    <div><div><ul><li><span class="icon icon-operation-removeBackground"></span></li></ul></div></div>
+  </div>
+</div>`;
