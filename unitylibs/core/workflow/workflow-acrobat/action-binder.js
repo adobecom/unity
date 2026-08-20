8ae1b93e5448287b3fd0a1489c9605d210b831d7
Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>
2026-08-20 12:45:26 -0700

    handle ims race condition

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -225,6 +225,7 @@ export default class ActionBinder {
       this.tokenError = result.error;
       return;
     }
+    this.tokenError = null;
     this.signedOut = result.isGuest ?? undefined;
   }
 
@@ -840,6 +841,7 @@ export default class ActionBinder {
     await this.loadTransitionScreen();
     await this.handlePreloads();
     if (this.signedOut === undefined) {
+      if (this.tokenError) await this.initialize();
       if (this.tokenError) {
         const errorDetails = this.tokenError;
         await this.dispatchErrorToast('pre_upload_error_fetching_access_token', null, `Could not fetch access token; Error: ${JSON.stringify(errorDetails.originalError)}`, false, true, {
