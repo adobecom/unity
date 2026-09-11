399b890f87133643674c02212b1f89ba4c9675cf
Arushi Gupta <arugupta@Arushis-MacBook-Pro-6.local>
2026-09-11 18:04:17 +0530

    Pointing BE to stage

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -312,9 +312,9 @@ export const unityConfig = (() => {
       ...commoncfg,
     },
     stage: {
-      apiEndPoint: 'https://unity-dev-ew1.adobe.io/api/v1',
-      connectorApiEndPoint: 'https://unity-dev-ew1.adobe.io/api/v1/asset/connector',
-      pageConfigEndPoint: 'https://cdn-unity.dev-ew1.adobe.com/api/v1/pageConfig',
+      apiEndPoint: 'https://unity-stage.adobe.io/api/v1',
+      connectorApiEndPoint: 'https://unity-stage.adobe.io/api/v1/asset/connector',
+      pageConfigEndPoint: 'https://cdn-unity.stage.adobe.com/api/v1/pageConfig',
       env: 'stage',
       ...commoncfg,
     },
